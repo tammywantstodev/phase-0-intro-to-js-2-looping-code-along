e84b8d0d6849624e3f5e085f79199d91412b3fab
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2024-11-20 17:27:54 +0300

    deleted unnecessary file

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const names=["Charlie", "Samip", "Ali"]
-let messages=[];
-// function writeCards(names, event="birthday"){
-    
-//     for(let i=0; i<names.length; i++){
-
-// const message=`Thank you, ${names[i]} for the wonderful ${event} gift.`
-// messages.push(message)
-//     }
-// return messages;
-// }
-// writeCards(names);
-// console.log(messages);
-
-let i=0
-function writeCards(names, event="birthday"){
-while(i<names.length){
-    const message=`Thank you, ${names[i]} for the wonderful ${event} gift.`
-    i++;
-messages.push(message)}
-return messages;
+function countdown(number){
+    let i=0
+    while(i<number){
+       console.log(number);
+      number=number-1
+    }
+    return number
 }
-writeCards(names, "Christmas");
-console.log(messages);
+
--- index1.js
+++
@@ -1,9 +0,0 @@
-function countdown(number){
-    let i=0
-    while(i<number){
-       console.log(number);
-      number=number-1
-    }
-    return number
-}
-
